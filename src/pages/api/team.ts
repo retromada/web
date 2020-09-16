6cdfab643fe2932af510df66dd040b047b6b34ba
tenasatupitsyn <214782@bk.ru>
2020-09-16 19:25:13 -0300

    Separate developer seek

--- src/pages/api/team.ts
+++ src/pages/api/team.ts
@@ -1,37 +1,45 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
-import Discord from '../../services/discord'
+import Discord from '../../services/Discord'
 
 let cachedTeam: [] = null
 
-export default async (request: NextApiRequest, response: NextApiResponse) => {
-  try {
-    if (cachedTeam) {
-      return response.status(200).json(cachedTeam)
-    }
+async function fetchTeamDevelopers (guildID = process.env.GUILD_ID) {
+  if (cachedTeam) {
+    return cachedTeam
+  }
 
-    const guildID = process.env.GUILD_ID
+  const roles = await Discord.request(`/guilds/${guildID}/roles`, {})
+  const members = await Discord.request(`/guilds/${guildID}/members`, { query: { limit: 1000 } })
 
-    const _roles = await Discord.request(`/guilds/${guildID}/roles`, {})
-    const members = await Discord.request(`/guilds/${guildID}/members`, { query: { limit: 1000 } })
+  const team = members
+    .filter(({ roles }) => roles.includes(process.env.COMMANDER_ROLE_ID))
+    .map((member) => {
+      const { user: { id, username, discriminator, avatar } } = member
 
-    const team = members
-      .filter(({ roles}) => roles.includes(process.env.COMMANDER_ROLE_ID))
-      .map(({ user, roles }) => ({
-        id: user.id,
-        username: user.username,
-        discriminator: user.discriminator,
-        avatar: user.avatar,
-        roles: roles
-          .filter((roleID) => ![process.env.OPERATOR_ROLE_ID].includes(roleID))
-          .map((roleID) => _roles.filter(({ id }) => id === roleID))
+      return {
+        id,
+        username,
+        discriminator,
+        avatar,
+        roles: member.roles
+          .filter((roleID) => process.env.OPERATOR_ROLE_ID !== roleID)
+          .map((roleID) => roles.filter(({ id }) => id === roleID))
           .flat()
           .map(({ id, name }) => ({ id, name }))
-      }))
+      }
+    })
 
-      cachedTeam = team
+    cachedTeam = team
 
-      return response.status(200).json(team)
+    return team
+}
+
+export default async (request: NextApiRequest, response: NextApiResponse) => {
+  try {
+    const team = await fetchTeamDevelopers()
+
+    return response.status(200).json(team)
   } catch (error) {
     return response.status(400).json({ message: error.message })
   }

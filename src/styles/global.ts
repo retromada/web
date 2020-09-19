fcd1b64bcfeea3e599a0c5e4f12982ac587609a2
tenasatupitsyn <214782@bk.ru>
2020-09-19 18:48:55 -0300

    Body margin as zero

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,9 +2,10 @@ import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
   body {
+    margin: 0;
     height: 100%;
     background: ${(props) => props.theme.colors.background};
     color: ${(props) => props.theme.colors.text};
-    font-family: Ebrima
+    font-family: Ebrima;
   }
 `

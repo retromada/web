9fb5679511b345314273c245bd05a67d3ce70efe
tenasatupitsyn <214782@bk.ru>
2021-01-27 01:12:56 -0300

    Linter

--- src/services/Discord.ts
+++ src/services/Discord.ts
@@ -1,26 +1,31 @@
-import fetcher from '../utils/fetcher'
 import { FetcherOptions } from '../types'
+import fetcher from '../utils/fetcher'
 
 const API_URL = 'https://discordapp.com/api'
 const { DISCORD_TOKEN, GUILD_ID } = process.env
 
 export default class Discord {
-  public static roles (guildID: string = GUILD_ID) {
+  public static roles (guildID: string = GUILD_ID): Promise<[]> {
     return this._request(`/guilds/${guildID}/roles`)
   }
 
-  public static members (guildID: string = GUILD_ID) {
-    return this._request(`/guilds/${guildID}/members`, { query: { limit: 1000 } })
+  public static members (guildID: string = GUILD_ID): Promise<[]> {
+    return this._request(`/guilds/${guildID}/members`, {
+      query: { limit: 1000 }
+    })
   }
 
-  public static _request (endpoint: string, {
-    query,
-    options = {
-      headers: {
-        Authorization: `Bot ${DISCORD_TOKEN}`
+  public static _request (
+    endpoint: string,
+    {
+      query,
+      options = {
+        headers: {
+          Authorization: `Bot ${DISCORD_TOKEN}`
+        }
       }
-    }
-  }: FetcherOptions = {}) {
+    }: FetcherOptions = {}
+  ): Promise<any> {
     return fetcher(API_URL + endpoint, { query, options })
   }
 }
